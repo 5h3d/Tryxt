bb1c63cefc08ae88f31be687e5c8313c3ab5ed87
Udeh Shedrack <udehshedrackugo@gmail.com>
2022-04-19 20:50:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { BsArrowRight } from "react-icons/bs";
+import { BiSend } from "react-icons/bi";
+import { abi, CONTRACT_ADDRESS } from "./constants";
+
+import { ethers } from "ethers";
+
+
 
 function App() {
+  const [ address, setAddress] = useState("0x000000000000000000")
+  const [ addressTo, setAddressTo] = useState(" ")
+  const [ amount, setAmount] = useState(" ")
+  const [ message, setMessage] = useState(" ")
+  // const [ balance, setBalance] = useState("24")
+  const [ history, setHistory] = useState(false)
+  const [ loading, setLoading] = useState(false)
+  const [ connected, setConnected] = useState(false)
+  // const [ provider, setProvder] = useState("")
+  const [ ethBalance, setEthBalance] = useState(0)
+  // const [ contract, setContract] = useState({})
+  
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const contracts = new ethers.Contract(CONTRACT_ADDRESS, abi, provider)
+
+ 
+  useEffect(()=>{
+    
+  }, [])
+
+  const connectWallet = async()=>{
+    try {
+      
+      
+      
+      
+      const account = await provider.send("eth_requestAccounts", [0]);
+      
+      const balance = await provider.getBalance(account[0]);
+      
+      setConnected(true)
+      setEthBalance(ethers.utils.formatEther(balance))
+
+      setAddress(account)
+      
+      // const aboutContract = new ethers.Contract(NFT_CONTRACT_ADDRESS, abi, provider)
+
+      // setContract(aboutContract);
+
+      // console.log(aboutContract)
+
+
+      
+      
+    } catch (error) {
+      // throw error ("connect to metamask")
+    }
+
+  }
+
+  console.log(contracts)
+  
+
+  const transfer = async()=>{
+    const signer = provider.getSigner()
+    const contracts = new ethers.Contract(CONTRACT_ADDRESS, abi, signer)
+    contracts.transferFun(addressTo, )
+    // console.log(contracts.transferFun)
+  }
+  transfer()
+
+
+  const getAllTransactions = async()=>{
+    
+    const contracts = new ethers.Contract(CONTRACT_ADDRESS, abi, provider)
+    console.log(contracts.getAllTransactions)
+  }
+
+  getAllTransactions()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-content">
+
+        {!connected? <button onClick={connectWallet}>Connect wallet</button> : ""}
+        
+
+        {connected && (<div className="wallet__details">
+          <div className="send__transaction">
+            <input type="text" placeholder='Address' onChange={(e)=>{ setAddressTo(e.target.value)}}/>
+            <input type="number" placeholder='Amount' onChange={(e)=>{ setAmount(e.target.value)}} />
+            <h3>{amount}</h3>
+            <input type="text" placeholder='message' onChange={(e)=>{ setMessage(e.target.value)}}/>
+            <button>Send <BiSend /></button>
+
+          </div>
+          <div className="card">
+            <div className="card__showcase">
+              <h4>{address}</h4>
+              <p>{ethBalance}ETH</p>
+            </div>
+            <p className="transaction__history-btn" onClick={()=>{setHistory(!history)}}>Transaction history</p>
+          </div>
+        </div>)}
+      </div>
+
+      { connected && history && (<div className="transaction__history ">
+        <div className="close"> <BsArrowRight  className='close__icon' onClick={()=>{setHistory(false)}} /></div>
+        <div className="transaction__header">
+          <h2>Transaction history</h2>
+        </div>
+          <div className="transaction__history-contents">
+            <div className="transaction__history-data">
+              <h3>To: 0xxxxxssxx</h3>
+              <p>Amount:</p>
+              <p>Message:</p>
+              <hr />
+            </div>
+            <div className="transaction__history-data">
+              <h3>To: 0xxxxxssxx</h3>
+              <p>Amount:</p>
+              <p>Message:</p>
+              <hr />
+            </div>
+            <div className="transaction__history-data">
+              <h3>To: 0xxxxxssxx</h3>
+              <p>Amount:</p>
+              <p>Message:</p>
+            </div>
+          </div>
+      </div>)}
     </div>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,117 @@
+export const abi = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "reciever",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "message",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "transferEvent",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "message",
+				"type": "string"
+			}
+		],
+		"name": "transferFun",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAllTransactionCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAllTransactions",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address",
+						"name": "sender",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "receiver",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "message",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "timestamp",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Transaction.transferStruct[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+export const CONTRACT_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138"
